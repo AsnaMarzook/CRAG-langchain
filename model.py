f5b24da624361aff48e1fe78e492253f8034470c
AsnaMarzook <fsasna2020@gmail.com>
2024-03-20 22:52:20 +0530

    model

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import os
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/home/thlurte/lab/EKB-API/app/experiments/temporal-bebop-411009-6fd3a81d0dc7.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"D:\GitHub\CRAG-langchain\temporal-bebop-411009-6fd3a81d0dc7.json"
 
 from google.auth import default
 
@@ -8,3 +8,12 @@ credentials, project_id = default()
 
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_google_vertexai import ChatVertexAI
+
+system = "You are a helpful assistant who translate English to Tamil"
+human = "Translate this sentence from English to Tamil. I love programming."
+prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+ 
+chat = ChatVertexAI()
+ 
+chain = prompt | chat
+print(chain.invoke({}))
